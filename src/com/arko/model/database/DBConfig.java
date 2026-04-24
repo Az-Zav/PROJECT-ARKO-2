427e3a62e25670e9db8cd07376b2523ccd702462
Zav <qvjcondicion@tip.edu.ph>
2026-04-24 22:43:14 +0800

    Format DBConfig.java for consistency

--- src/com/arko/model/database/DBConfig.java
+++ src/com/arko/model/database/DBConfig.java
@@ -17,11 +17,11 @@ public final class DBConfig {
 
     private DBConfig() {}
 
-    public static final String HOST     = "localhost";         //online db: "mysql-25df71ba-project-4ark0.j.aivencloud.com";
-    public static final int    PORT     = 3306;                //           23021;
+    public static final String HOST     = "localhost";      
+    public static final int    PORT     = 3306;              
     public static final String DATABASE = "project_arko_db";
-    public static final String USER     = "root";              //           "avnadmin";
-    public static final String PASSWORD = "rootPwSql";         //           "AVNS_jtujNQHkcGQTqFWnxO-";
+    public static final String USER     = "root";             
+    public static final String PASSWORD = "rootPwSql";       
 
     // Assembled URL — do not modify
     public static final String URL =
